cbbd162ef159fc7af5523431104e545d408f2221
Alex-Park-CS <alexpark0930@gmail.com>
2023-11-05 21:04:11 -0800

    changes in desktop

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,18 +1,18 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-// var firebaseConfig = {
-//     apiKey:
-//     authDomain:
-//     projectId:
-//     storageBucket:
-//     messagingSenderId:
-//     appId:
-// };
+var firebaseConfig = {
+    apiKey: "AIzaSyBbEreOFo005aKp_nkT9e3mWReP7C6Av3w",
+    authDomain: "comp1800-demo-202330-projects.firebaseapp.com",
+    projectId: "comp1800-demo-202330-projects",
+    storageBucket: "comp1800-demo-202330-projects.appspot.com",
+    messagingSenderId: "729312877969",
+    appId: "1:729312877969:web:9a64d8cde4e0ae7787ce30"
+};
 
 //--------------------------------------------
 // initialize the Firebase app
 // initialize Firestore database if using it
 //--------------------------------------------
-// const app = firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore();
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
