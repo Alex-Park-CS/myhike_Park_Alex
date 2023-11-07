006674713dad483772c1eede5e7642e2d873ed38
Alex-Park-CS <alexpark0930@gmail.com>
2023-11-07 10:50:56 -0800

    done

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,18 +1,19 @@
-//----------------------------------------
-//  Your web app's Firebase configuration
-//----------------------------------------
-var firebaseConfig = {
-    apiKey: "AIzaSyBbEreOFo005aKp_nkT9e3mWReP7C6Av3w",
-    authDomain: "comp1800-demo-202330-projects.firebaseapp.com",
-    projectId: "comp1800-demo-202330-projects",
-    storageBucket: "comp1800-demo-202330-projects.appspot.com",
-    messagingSenderId: "729312877969",
-    appId: "1:729312877969:web:9a64d8cde4e0ae7787ce30"
-};
+// import { initializeApp } from "firebase/app";
+// //----------------------------------------
+// //  Your web app's Firebase configuration
+// //----------------------------------------
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBbEreOFo005aKp_nkT9e3mWReP7C6Av3w",
+//     authDomain: "comp1800-demo-202330-projects.firebaseapp.com",
+//     projectId: "comp1800-demo-202330-projects",
+//     storageBucket: "comp1800-demo-202330-projects.appspot.com",
+//     messagingSenderId: "729312877969",
+//     appId: "1:729312877969:web:9a64d8cde4e0ae7787ce30"
+// };
 
-//--------------------------------------------
-// initialize the Firebase app
-// initialize Firestore database if using it
-//--------------------------------------------
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// //--------------------------------------------
+// // initialize the Firebase app
+// // initialize Firestore database if using it
+// //--------------------------------------------
+// const app = firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore();
